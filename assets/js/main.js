3e327ce7dad9b227b864ade29b2fa7215834afae
RyanSH <ryanhilario5@hotmail.com>
2023-09-21 00:24:29 -0300

    criação de animação de mostrar/ocultar itens do menu (Conhecimentos)

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,45 @@ const navMenu = document.getElementById("nav-menu")
 const navToggle = document.getElementById("nav-toggle")
 const navClose = document.getElementById("nav-close")
 
-// toggle.addEventListener("click", () => {
-// 	menu.style.bottom = 0
-// })
-
-// navClose.addEventListener("click", () => {
-// 	menu.style.bottom = "-100%"
-// })
-
 if(navToggle) {
-	navToggle.addEventListener('click', () => {
-		navMenu.classList.add('show-menu')
+	navToggle.addEventListener("click", () => {
+		navMenu.classList.add("show-menu")
 	})
-} 
+};
 
-
-const navLink = document.querySelectorAll('.nav__link')
 
 function removeMenu() {
-	navMenu.classList.remove('show-menu')
-}
+	navMenu.classList.remove("show-menu")
+};
 
 if(navClose) {
-	navClose.addEventListener('click', removeMenu)
+	navClose.addEventListener("click", removeMenu)
+};
+
+// ============= OCULTANDO MENU MOBILE AO CLICKAR EM ITEM =============
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach(n => n.addEventListener("click", removeMenu));
+
+
+
+// ============= MOSTRANDO/OCULTANDO MENUS CONHECIMENTOS AO CLICKAR =============
+const skillsContent = document.getElementsByClassName("skills__content")
+const skillsHeader = document.querySelectorAll(".skills__header")
+
+function toggleSkills() {
+	let itemClass = this.parentNode.className
+
+	for (let i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className = "skills__content skills__close";	
+	}
+
+	if(itemClass === "skills__content skills__close") {
+		this.parentNode.className = "skills__content skills_open"
+	}
 }
 
-// ============= OCULTANDO MENU MOBILE AO CLICKAR =============
-navLink.forEach(n => n.addEventListener('click', removeMenu))
+skillsHeader.forEach((el) => {
+	el.addEventListener("click", toggleSkills)
+})
+
