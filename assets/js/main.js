e56723a292780912cf45e54a75a9544c6380c8c0
RyanSH <ryanhilario5@hotmail.com>
2023-10-18 16:56:35 -0300

    finalizando seção (Serviços)

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,25 @@ skillsHeader.forEach((skill) => {
 	skill.addEventListener("click", toggleSkills)
 })
 
+
+
+// ============= SERVIÇOS TABS =============
+const tabs = document.querySelectorAll("[data-target")
+const tabContent = document.querySelectorAll("[data-content");
+
+tabs.forEach(tab => {
+	tab.addEventListener("click", ()=> {
+		const target = document.querySelector(tab.dataset.target)
+
+		tabContent.forEach(tabContent => {
+			tabContent.classList.remove("qualification__active")
+		})
+		target.classList.add("qualification__active")
+
+		tabs.forEach(tab => {
+			tab.classList.remove("qualification__active")
+		})
+
+		tab.classList.add("qualification__active")
+	})
+})
