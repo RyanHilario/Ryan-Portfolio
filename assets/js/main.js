66e98624093d82b36ab978ac386797b5dad7ede5
RyanSH <ryanhilario5@hotmail.com>
2023-11-07 12:48:52 -0300

    correção dos arquivos Swiper Css e Swiper Js/ correção dos links internos Swiper Css e Swiper Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,19 @@ modalCloses.forEach((modalClose) => {
 		})
 	})
 })
+
+
+
+// ============= SWIPER PROJETOS =============
+let swiper = new Swiper(".projects__container", {
+	cssMode: true,
+	navigation: {
+	  	nextEl: ".swiper-button-next",
+	  	prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+	  	el: ".swiper-pagination",
+	},
+	mousewheel: true,
+	keyboard: true,
+});
